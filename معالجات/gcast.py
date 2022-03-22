0efe1786d2d05431fd94111874177f134f989be6
sasaal8 <101926168+sasaal8@users.noreply.github.com>
2022-03-22 23:17:37 +0200

    Update and rename handlers/gcast.py to معالجات/gcast.py

--- معالجات/gcast.py
+++ معالجات/gcast.py
@@ -8,24 +8,24 @@ from pyrogram.errors import UserAlreadyParticipant
 from modules.callsmusic.callsmusic import client as aditya
 from modules.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
+@Client.on_message(filters.command(["اذاعه"]))
 async def broadcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Stɑɤtɩŋʛ Ɓɤøɑɗƈɑst ...`")
+        wtf = await message.reply("`بدء الاذاعه♥ ...`")
         if not message.reply_to_message:
-            await wtf.edit("**__Ƥɭɘɑsɘ Ʀɘƥɭy Ƭø ɑ Mɘssɑʛɘ Ƭø Stɑɤt Ɓɤøɑɗƈɑst ...__**")
+            await wtf.edit("**__من فضـلك قـم بالرد علي رسـاله لاذعـاتها...__**")
             return
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
             try:
                 await aditya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Ɓɤøɑɗƈɑstɩŋʛ` \n\n**Sɘŋt Ƭø:** `{sent}` Ƈɦɑts \n**Fɑɩɭɘɗ Iŋ:** {failed} chats")
+                await wtf.edit(f"`احصائيات` \n\n**تم الارسـال الي:** `{sent}` مجموعه \n**فشل الارسال في:** {failed} مجموعه")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Ɠƈɑst Sʋƈƈɘssfʋɭɭy` \n\n**Sɘŋt Ƭø:** `{sent}` Ƈɦɑts \n**Fɑɩɭɘɗ Iŋ:** {failed} Ƈɦɑts")
+        await message.reply_text(f"`احصائيات \n\n**تـم الارسال الي:** `{sent} مجموعه \n**فشل الارسال في:** {failed} مجموعه")
